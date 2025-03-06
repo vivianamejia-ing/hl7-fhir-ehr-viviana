11dcc1f5ca0e70bf05c0f32759582a180b6598aa
Mario D. Gonzalez <mario.gonzalez@leapmetrics.io>
2025-03-06 00:09:09 -0500

    Returning a JSON

--- app/app.py
+++ app/app.py
@@ -14,7 +14,11 @@ def get_patient_by_id(patient_id: str):
     patient = collection.find_one({"_id": ObjectId(patient_id)})
     if patient:
         patient["_id"] = str(patient["_id"])
-        return patient  # Return patient
+        pat = Patient.model_validate_json(patient)
+        if pat.get_resource_type() == "Patient":
+            return patient  # Return patient
+        else:
+            raise HTTPException(status_code=401, detail="Patient not well formatted")
     else:
         raise HTTPException(status_code=404, detail="Patient not found")
 
