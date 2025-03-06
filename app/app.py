998f433b463b1e702bcb5dc47069e0d586b02217
Mario D. Gonzalez <mario.gonzalez@leapmetrics.io>
2025-03-05 23:30:44 -0500

    Fixing ObjectId

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException
 import uvicorn
 from fhir.resources.patient import Patient
 from connection import connect_to_mongodb
+from bson import ObjectId 
 
 app = FastAPI()
 
@@ -9,11 +10,11 @@ collection = connect_to_mongodb("SamplePatientService", "patients")
 
 @app.get("/patient/{patient_id}", response_model=Patient)
 def get_patient_by_id(patient_id: str):
-    patient = collection.find_one({"_id": patient_id})
+    patient = collection.find_one({"_id": ObjectId(patient_id)})
     if patient:
         return Patient(**patient)
     else:
-        raise HTTPException(status_code=404, detail="Paciente no encontrado")
+        raise HTTPException(status_code=404, detail="Patient not found")
 
 
 if __name__ == '__main__':
